fa435e6fe5abac0f180e43d74c94ccb751b5eaee
sushmi5keka <sushmi5keka@gmail.com>
2019-11-15 21:14:35 +0600

    ok

---
+++ TourMate/app/src/main/java/com/israt/tourmate/Activity/MainActivity.java
@@ -0,0 +1,111 @@
+package com.israt.tourmate.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.israt.tourmate.Fragment.DashboardFragment;
+import com.israt.tourmate.Fragment.MapFragment;
+import com.israt.tourmate.Fragment.TripsFragment;
+import com.israt.tourmate.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("Dashboard");
+
+        init();
+
+        fragment(new DashboardFragment());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navBar);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+
+                    case R.id.dashboard:
+                        fragment(new DashboardFragment());
+                        setTitle("Dashboard");
+                        return true;
+                    case R.id.trips:
+                        fragment(new TripsFragment());
+                        setTitle("Trips");
+                        return true;
+                    case R.id.map:
+                        fragment(new MapFragment());
+                        setTitle("Map");
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void init() {
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public Fragment fragment(Fragment fragment) {
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragmentView, fragment);
+        ft.commit();
+        return fragment;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater mf = getMenuInflater();
+        mf.inflate(R.menu.top_right_manu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.profile:
+                Toast.makeText(this, "Help", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.logout:
+                firebaseAuth.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+                break;
+            case R.id.exit:
+                firebaseAuth.signOut();
+                finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
+
---
+++ TourMate/app/src/main/java/com/israt/tourmate/Activity/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.israt.tourmate.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.israt.tourmate.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+                finish();
+            }
+        },5000);
+
+    }
+}
